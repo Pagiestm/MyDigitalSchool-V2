b8ba88c98a46c984a469bb1937e64c26a8328626
Lucas Madranges <lucas.madranges.pro@icloud.com>
2024-06-04 20:52:55 +0200

    feat: changing colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState} from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  return (
-    <>
-        <h1 className="text-3xl font-bold text-red-800 underline">
-          Hello world!
-        </h1>
-    </>
-  )
+    return (
+        <>
+            <h1 className="text-3xl font-bold text-red-800 underline">
+                Hello world!
+            </h1>
+        </>
+    );
 }
 
-export default App
+export default App; 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "blue-aqua": "#2DB7C4",
+        "Cyan": "#29A5B0",
+        "black": "#181818",
+        "laser-lemon": "#FFBD1D",
+      }
+    },
   },
   plugins: [],
 }
