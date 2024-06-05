a691d954c6f9821735dd7633f0f0382f417bbf76
Lucas Bertaud <l.bertaud@hotmail.com>
2024-06-05 19:59:59 +0200

    style(Ecole): laptop and desktop style

--- src/components/pages/Ecole.jsx
+++ src/components/pages/Ecole.jsx
@@ -1,15 +1,19 @@
 export default function Ecole() {
   return (
       <main className="mt-24 md:px-44">
-        <h1 className="text-2xl px-5 font-bold font-sora mb-4 text-blue-aqua text-center">MyDigitalSchool, l’école du multimédia</h1>
+        <h1 className="text-xl xl:text-2xl px-5 font-bold font-sora mb-4 text-blue-aqua text-center">MyDigitalSchool, <br/> l’école du multimédia</h1>
         <div className="w-full rounded-lg flex justify-center">
-          <img className="object-cover object-center w-full h-full md:rounded-md"
+          <img className="object-cover object-center w-full h-full lg:w-1/2 lg:h-1/2 md:rounded-md"
             src="https://www.lilletourism.com/app/uploads/lille-tourisme/2023/09/thumbs/Grand-Place-Lille-Tourisme-23-min-1920x960-crop-1695644063.jpg"
             alt="stats" />
         </div>
-        <p className="mt-4 px-5 md:px-0">Sa taille humaine pour l’accompagnement personnalisé des élèves et l’accent mis sur la professionnalisation. Notre objectif est que chaque étudiant approfondisse son projet professionnel parmi toutes les voies du numérique et s’épanouisse dans le métier de son choix. Le taux d’insertion des élèves des écoles MyDigitalSchool est de 95% après le diplôme !
-          L'école met à disposition des étudiants tous les outils multimédias nécessaires à leur formation. Des outils de design, de développement web ou même d'apprentissages des langues étrangères essentielles dans les métiers digitaux.
-          Un point d'honneur est mis à l'apprentissage du web et du multimédia pour les étudiants non issus de formations dans le domaine du numérique.</p>
+        <div className="flex justify-center">
+          <p className="mt-4 px-5 md:px-0 lg:w-2/3">
+            Sa taille humaine pour l’accompagnement personnalisé des élèves et l’accent mis sur la professionnalisation. Notre objectif est que chaque étudiant approfondisse son projet professionnel parmi toutes les voies du numérique et s’épanouisse dans le métier de son choix. Le taux d’insertion des élèves des écoles MyDigitalSchool est de 95% après le diplôme !
+            L'école met à disposition des étudiants tous les outils multimédias nécessaires à leur formation. Des outils de design, de développement web ou même d'apprentissages des langues étrangères essentielles dans les métiers digitaux.
+            Un point d'honneur est mis à l'apprentissage du web et du multimédia pour les étudiants non issus de formations dans le domaine du numérique.
+          </p>
+        </div>
       </main>
   );
 }
