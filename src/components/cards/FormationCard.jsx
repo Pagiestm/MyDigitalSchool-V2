b01bc699000bc78c56bc3a6d503c1da38ccb30d1
pagiestm <pagiestm@gmail.com>
2024-06-08 21:41:29 +0200

    fix(FormationCard): change balise "a" for "link"

--- src/components/cards/FormationCard.jsx
+++ src/components/cards/FormationCard.jsx
@@ -1,17 +1,18 @@
-export default function FormationCard({title, description, icon, href}) {
+import { Link } from 'react-router-dom';
+
+export default function FormationCard({ title, description, icon, href }) {
     return (
         <div className="lg:w-1/2 md:w-full mb-6">
             <div className="flex rounded-lg border-gray-200 border-opacity-50 sm:flex-row flex-row">
                 <div className="w-12 h-12 md:w-16 md:h-16 mr-4 md:mr-8 sm:mb-0 mb-4 inline-flex items-center justify-center rounded-full bg-black text-indigo-500 flex-shrink-0">
-                    <img className="w-8 lg:w-11" src={icon}/>
+                    <img className="w-8 lg:w-11" src={icon} alt="icon"/>
                 </div>
                 <div className="flex-grow">
                     <h2 className="text-gray-900 text-lg font-semibold font-sora mb-2">{title}</h2>
                     <p className="leading-relaxed text-base line-clamp-4">{description}</p>
-                    <a href={`/formations?id=${href}`}
-                       className="mt-3 bg-blue-aqua text-black py-1 px-4 rounded-full inline-flex items-center hover:cursor-pointer hover:bg-Cyan">En
-                        savoir
-                        plus
+                    <Link to={`/formations?id=${href}`}
+                          className="mt-3 bg-blue-aqua text-black py-1 px-4 rounded-full inline-flex items-center hover:cursor-pointer hover:bg-Cyan">
+                        En savoir plus
                         <svg fill="none"
                              stroke="currentColor"
                              strokeLinecap="round"
@@ -21,7 +22,7 @@ export default function FormationCard({title, description, icon, href}) {
                              viewBox="0 0 24 24">
                             <path d="M5 12h14M12 5l7 7-7 7"></path>
                         </svg>
-                    </a>
+                    </Link>
                 </div>
             </div>
         </div>
